1d024fbd62ac9bce47607fea89ecc3d3d390bb5b
shunningtong285 <shunningtong285@gmail.com>
2026-04-14 16:45:27 +0800

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCx_BLIWVKZs0vJa5TwL6zoycJexY_5nXU",
-  authDomain: "system-1e90a.firebaseapp.com",
-  databaseURL: "https://system-1e90a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "system-1e90a",
-  storageBucket: "system-1e90a.firebasestorage.app",
-  messagingSenderId: "80947900109",
-  appId: "1:80947900109:web:b6cd62bb2f1e07971a4384"
+  apiKey: "AIzaSyBiHsQcLzBdVf6mi1-upp57t-RnpiZj01k",
+  authDomain: "shunningtongsystem.firebaseapp.com",
+  databaseURL: "https://shunningtongsystem-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shunningtongsystem",
+  storageBucket: "shunningtongsystem.firebasestorage.app",
+  messagingSenderId: "1039030570186",
+  appId: "1:1039030570186:web:3a81c623d0fcc3339f3833"
 };
 
 export default firebaseConfig;
